390e9990b0f279c1cfce028e2a73c04d73e6faf0
githubrimam <rimam6212@gmail.com>
2024-07-03 18:43:06 +0530

    msg

---
+++ js/app.js
@@ -0,0 +1,116 @@
+function toggleMenu() {
+    const navList = document.querySelector('.navbar-list');
+    navbarList.style.display = navbarList.style.display === 'flex';
+}
+
+//add event on element
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+  document.body.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+
+const activeHeader = function () {
+  if (window.scrollY > 300) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeHeader);
+
+
+
+/**
+ * toggle active on add to fav
+ */
+
+const addToFavBtns = document.querySelectorAll("[data-add-to-fav]");
+
+const toggleActive = function () {
+  this.classList.toggle("active");
+}
+
+addEventOnElem(addToFavBtns, "click", toggleActive);
+
+
+
+/**
+ * scroll revreal effect
+ */
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 1.5) {
+      sections[i].classList.add("active");
+    } else {
+      sections[i].classList.remove("active");
+    }
+  }
+}
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
+function validateForm() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const courses = document.getElementById('courses').value;
+
+  if (name === '' || email === '' || phone === '' || courses === '') {
+      alert('Please fill out all fields.');
+      return false;
+  }
+
+  const phonePattern = /^[0-9]{10}$/;
+  if (!phonePattern.test(phone)) {
+      alert('Please enter a valid 10-digit phone number.');
+      return false;
+  }
+
+  alert('Enrollment Successful!');
+  return true;
+}
